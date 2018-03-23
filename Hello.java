1f1c58d9985ccc62d94466eeb8349c9d0e882e9c
Jain <himanshu.jain@capgemini.com>
2018-03-23 15:58:45 -0500

    added hello world program

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package com.himanshu.jenkinstest;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello Jenkins "+ i);
+		}
+	}
+}
